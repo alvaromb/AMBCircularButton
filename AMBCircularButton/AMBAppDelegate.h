699b61f7afe8e2b06ed1a72ee3328f6f044246f5
alvaro <alvaro@mola.com>
2014-02-28 12:44:40 +0100

    First commit

---
+++ AMBCircularButton/AMBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMBAppDelegate.h
+//  AMBCircularButton
+//
+//  Created by amb on 28/02/14.
+//  Copyright (c) 2014 AMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AMBCircularButton/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AMBCircularButton
+//
+//  Created by amb on 28/02/14.
+//  Copyright (c) 2014 AMB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AMBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AMBAppDelegate class]));
+    }
+}
